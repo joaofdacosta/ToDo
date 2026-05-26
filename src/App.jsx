e73cb73abc910e39a2344c4eaf38d9f1c2216478
joaofdacosta <joaofc88@gmail.com>
2026-05-26 10:02:11 -0300

    Testando a esteira

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ export default function App() {
 
   return (
     <main>
-      <h1>Carrinho Antes - Loja Saldão Do Baú</h1>
+      <h1>Carrinho Novo - Loja Saldão das notas</h1>
 
       <div>
         <div>
